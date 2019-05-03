3cb683d4a5ee390337763904dca9392b25af3a0a
Kylie Pace <kylie.pace@playsportsnetwork.com>
2019-05-03 16:40:52 +0100

    added geojson to mongo and added get endpoint for map to load darkness polygon

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ const http = require('http').Server(app);
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) { 
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+  next(); 
+});
 
 app.use('/api', routes);
 app.use('/', (req, res) => {
   res.end('helloooo')  
 });
+
 
 // start the database and then the server
 initDB((err, database) => {
--- routes/routes.js
+++ routes/routes.js
@@ -4,10 +4,19 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     // database connection available at req.app.locals.db
     const db = req.app.locals.db;
-    db.collection('geojson').insertOne({name: 'test'}, (err, r) => {
-        console.log(r)
-    })
+    // db.collection('geojson').insertOne({name: 'test'}, (err, r) => {
+    //   console.log('added to database')
+    //     console.log(r)
+    // })
     next();
+});
+
+router.get('/darkness', (req, res) => {
+  const db = req.app.locals.db;
+  return db.collection('geojson').findOne({name: 'darkness'}, (err, geojson) => {
+    console.log(geojson);
+    res.json(geojson);
+  });
 });
 
 module.exports = router;
